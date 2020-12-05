8e5d9ae8fc2d11bbbecd01b94266a39a0db9a3f9
vinaysreddy <62655250+vinaysreddy@users.noreply.github.com>
2020-12-05 21:15:26 +0530

    initial scrips done
    movement,colissions

---
+++ Carrabit/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]float speed = 0f;
+    SpriteRenderer sp;
+    [SerializeField] float maxPos=0f;
+    bool isMove = true;
+    void Start()
+    {
+        sp = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(isMove)
+        {
+            Move();
+        }
+        
+        void Move()
+        {
+            float inputX = Input.GetAxis("Horizontal");
+             transform.position += Vector3.right * inputX * speed * Time.deltaTime;
+
+            float cl=Mathf.Clamp(transform.position.x, -maxPos, maxPos);
+            transform.position=new Vector3(cl,transform.position.y, transform.position.z);
+
+            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+                sp.flipX = true;
+            else
+                sp.flipX = false;
+        }
+
+
+    }
+}
